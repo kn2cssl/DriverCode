2b358404868a4472d71bb6e50a165df2f2def68d
minamobahi <minamobahi@yahoo.com>
2014-08-22 16:48:25 +0430

    ok

--- driver/driver.c
+++ driver/driver.c
@@ -172,37 +172,6 @@ TIMSK1=0x00;
 // Timer/Counter 2 Interrupt(s) initialization
 TIMSK2=0x00;
 
-if (slave_address==0)
-{
-	// USART initialization
-	// Communication Parameters: 8 Data, 1 Stop, No Parity
-	// USART Receiver: On
-	// USART Transmitter: On
-	// USART0 Mode: Asynchronous
-	// USART Baud Rate: 9600
-	UCSR0A=0x00;
-	UCSR0B=0x98;
-	UCSR0C=0x06;
-	UBRR0H=0x00;
-	UBRR0L=0x33;
-}
-else
-{
-	// USART initialization
-	// Communication Parameters: 8 Data, 1 Stop, No Parity
-	// USART Receiver: On
-	// USART Transmitter: Off
-	// USART0 Mode: Asynchronous
-	// USART Baud Rate: 9600
-	UCSR0A=0x00;
-	UCSR0B=0x90;
-	UCSR0C=0x06;
-	UBRR0H=0x00;
-	UBRR0L=0x33;
-}
-
-
-
 
 // Analog Comparator initialization
 // Analog Comparator: Off
@@ -242,6 +211,35 @@ TWCR=0x00;
 //#pragma optsize+
 //#endif
 slave_address=ADD0|(ADD1<<1);
+
+if (slave_address==0)
+{
+	// USART initialization
+	// Communication Parameters: 8 Data, 1 Stop, No Parity
+	// USART Receiver: On
+	// USART Transmitter: On
+	// USART0 Mode: Asynchronous
+	// USART Baud Rate: 9600
+	UCSR0A=0x00;
+	UCSR0B=0x98;
+	UCSR0C=0x06;
+	UBRR0H=0x00;
+	UBRR0L=0x33;
+}
+else
+{
+	// USART initialization
+	// Communication Parameters: 8 Data, 1 Stop, No Parity
+	// USART Receiver: On
+	// USART Transmitter: Off
+	// USART0 Mode: Asynchronous
+	// USART Baud Rate: 9600
+	UCSR0A=0x00;
+	UCSR0B=0x90;
+	UCSR0C=0x06;
+	UBRR0H=0x00;
+	UBRR0L=0x33;
+}
 // Global enable interrupts
 asm("sei");
 DDRC|=(1<<PINC5);
@@ -274,7 +272,7 @@ DDRC|=(1<<PINC5);
 	       PORTC=PORTC|(1<<PINC5);
         }
 
-        pwm=80;
+        //pwm=80;
 		//Motor_Direction= 0;
         //        M1p=0;
         //        M2p=0;
@@ -418,7 +416,7 @@ if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
 		{
 			//LED_2  (~READ_PIN(PORTB,4));
 			asm("wdr");
-			//master_setpoint = tmp_setpoint;
+			master_setpoint = tmp_setpoint;
 			if (ask_tmp==slave_address)
 			{   //LED_1  (~READ_PIN(PORTB,0));
 				flg_ask=1;
@@ -433,16 +431,10 @@ if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
 
 ISR(PCINT2_vect)
 {
-	         Motor_Update(pwm,Motor_Direction);
-	         //if(HALL1 == 1)
-	         //{
-	         //WRITE_PORT(PORTD,1, HALL2);}
-			 //////////////////////////////////////for current sensing driver that sends direction on pin c.5
-			 //if(slave_address==0 || slave_address==1)
-			 //{
+	         Motor_Update(pwm,Motor_Direction);	         
 			 if(HALL1 == 1)
 			 {
-			 WRITE_PORT(PORTC,5, HALL2);}//}
+			 WRITE_PORT(PORTC,4, HALL2);}
 }
 void send_reply(void)
 {   
